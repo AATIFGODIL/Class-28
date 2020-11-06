84ef2f30b071a549ccd4e6bb76496251512109a6
aatifgodil <matin@Matins-MacBook-Air.local>
2020-11-06 14:32:50 +0530

    slingshot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+//var constrainedLog;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,6 +18,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    
+    
+    //constrainedLog = new Log(230,180,80,PI/2);
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,7 +41,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    slingshot = new Slingshot(bird.body,{x:200,y:100});
+    
 }
 
 function draw(){
@@ -61,4 +68,16 @@ function draw(){
 
     bird.display();
     platform.display();
+    //constrainedLog.display();
+    slingshot.display();
+    //line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
+    
+//chain.display();
+}
+function mouseDragged(){
+Matter.Body.setPosition(bird.body, {x: mouseX,y: mouseY})
+}
+
+function mouseReleased(){
+ slingshot.fly();   
 }
